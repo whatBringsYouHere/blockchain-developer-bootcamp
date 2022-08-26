ed9465657aa24abb39c018f33054def645af1cda
Eddie Aguilar <eddieaguilar@Eddies-MacBook-Pro.local>
2022-08-25 17:01:30 -0700

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,10 +11,17 @@ contract Token {
 
 	// Track Balances
 	mapping(address => uint256) public balanceOf;
+	mapping( address => mapping(address => uint256)) public allowance;
 
 	event Transfer(
 		address indexed from, 
 		address indexed to, 
+		uint256 value
+	);
+
+	event Approval (
+		address indexed owner,
+		address indexed spender,
 		uint256 value
 	);
 
@@ -29,19 +36,31 @@ contract Token {
 
 	function transfer(address _to, uint256 _value) public returns(bool success) {
 
-		// Require that sender has enough tokens to spend
 		require(balanceOf[msg.sender] >= _value);
 		require(_to != address(0));
 
-		// Deduct token from spender
 		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-		// Credit tokens to reciever
 		balanceOf[_to] = balanceOf[_to] + _value;
 
-		// Emit event
 		emit Transfer(msg.sender, _to, _value);
 
 		return true;
+	}
+
+	function approve(address _spender, uint256 _value) 
+	public 
+	returns (bool success)
+	{
+		require(_spender != address(0));
+
+		allowance[msg.sender][_spender] = _value;
+
+		emit Approval(msg.sender, _spender, _value);
+		return true;
 
 	}
+
+
+
+
 }
