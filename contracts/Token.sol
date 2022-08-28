4d03bd300c862e31ec48c15f0d744e99e821fab7
Eddie Aguilar <eddieaguilar@Eddies-MacBook-Pro.local>
2022-08-28 14:15:58 -0700

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,66 +1,92 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract Token {
-	string public name;
-	string public symbol;
-	uint256 public decimals = 18;
-	uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
 
-	// Track Balances
-	mapping(address => uint256) public balanceOf;
-	mapping( address => mapping(address => uint256)) public allowance;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-	event Transfer(
-		address indexed from, 
-		address indexed to, 
-		uint256 value
-	);
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
 
-	event Approval (
-		address indexed owner,
-		address indexed spender,
-		uint256 value
-	);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 
-	// Send Tokens
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
-		name = _name;
-		symbol = _symbol;
-		totalSupply = _totalSupply * (10 ** decimals);
-		balanceOf[msg.sender] = totalSupply;
-	}
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
 
-	function transfer(address _to, uint256 _value) public returns(bool success) {
+        _transfer(msg.sender, _to, _value);
 
-		require(balanceOf[msg.sender] >= _value);
-		require(_to != address(0));
+        return true;
+    }
 
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-		balanceOf[_to] = balanceOf[_to] + _value;
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
 
-		emit Transfer(msg.sender, _to, _value);
+        balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
 
-		return true;
-	}
+        emit Transfer(_from, _to, _value);
+    }
 
-	function approve(address _spender, uint256 _value) 
-	public 
-	returns (bool success)
-	{
-		require(_spender != address(0));
+    function approve(address _spender, uint256 _value)
+        public
+        returns(bool success)
+    {
+        require(_spender != address(0));
 
-		allowance[msg.sender][_spender] = _value;
+        allowance[msg.sender][_spender] = _value;
 
-		emit Approval(msg.sender, _spender, _value);
-		return true;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
 
-	}
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success)
+    {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
 
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
+        _transfer(_from, _to, _value);
 
+        return true;
+    }
 
 }
