faac59413c75fc8d89f0ddb4aaf1765399d6b280
Eddie Aguilar <developered95@proton.me>
2022-10-20 00:30:06 -0700

    28. My transactions

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -47,7 +47,7 @@ export const myOpenOrdersSelector = createSelector (
 	// Decorate orders - add display attributes
 	orders = decorateMyOpenOrders(orders, tokens)
 
-	// Decorate orders by date descending
+	// Sort orders by date descending
 	orders = orders.sort((a, b) => b.timestamp - a.timestamp)
 
 	return orders
@@ -177,6 +177,63 @@ const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
 		return RED //Danger
 	}
 }
+//--------------------------------------------------
+// MY FILLED ORDERS
+
+
+export const myFilledOrdersSelector = createSelector(
+    account,
+    tokens,
+    filledOrders,
+    (account, tokens, orders) => {
+      if (!tokens[0] || !tokens[1]) { return }
+
+      // Find our orders
+      orders = orders.filter((o) => o.user === account || o.creator === account)
+      // Filter orders for current trading pair
+      orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+      orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+      // Sort by date descending
+      orders = orders.sort((a, b) => b.timestamp - a.timestamp)
+
+      // Decorate orders - add display attributes
+      orders = decorateMyFilledOrders(orders, account, tokens)
+
+      return orders
+  }
+)
+
+const decorateMyFilledOrders = (orders, account, tokens) => {
+  return(
+    orders.map((order) => {
+      order = decorateOrder(order, tokens)
+      order = decorateMyFilledOrder(order, account, tokens)
+      return(order)
+    })
+  )
+}
+
+const decorateMyFilledOrder = (order, account, tokens) => {
+  const myOrder = order.creator === account
+
+  let orderType
+  if(myOrder) {
+    orderType = order.tokenGive === tokens[1].address ? 'buy' : 'sell'
+  } else {
+    orderType = order.tokenGive === tokens[1].address ? 'sell' : 'buy'
+  }
+
+  return({
+    ...order,
+    orderType,
+    orderClass: (orderType === 'buy' ? GREEN : RED),
+    orderSign: (orderType === 'buy' ? '+' : '-')
+  })
+}
+
+
+
 
 
 
